8c95d9256afeaea649b6b21150339243b9275a11
EsunR <641411169@qq.com>
2019-01-31 20:11:34 +0800

    update https

--- serve/serve.js
+++ serve/serve.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressRoute = require('express-route');
+const path = require('path')
+const fs = require('fs')
+const https = require('https')
+
+// 根据项目的路径导入生成的证书文件
+const privateKey = fs.readFileSync(path.join(__dirname, './certificate/private.key'), 'utf8')
+const certificate = fs.readFileSync(path.join(__dirname, './certificate/certificate.crt'), 'utf8')
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+}
 
 
 var server = express();
-server.listen(8080);
 
-//1.获取请求数据
-//get自带
-//post数据调用中间件body-parser
+// 创建https服务器实例
+const httpsServer = https.createServer(credentials, server)
+// 设置https的访问端口号
+const SSLPORT = 8080
+
+// 启动服务器，监听对应的端口
+httpsServer.listen(SSLPORT, () => {
+  console.log(`HTTPS Server is running on: https://localhost:${SSLPORT}`)
+})
+
+//获取请求数据
 server.use(bodyParser.urlencoded());
 server.use(bodyParser.json({limit: '1mb'}));
 server.use(bodyParser.urlencoded({
@@ -16,6 +33,6 @@ server.use(bodyParser.urlencoded({
 }));
 
 
-//4.router
+// router
 server.use('/api/werewolves', require('./route/werewolves.js')());
 
